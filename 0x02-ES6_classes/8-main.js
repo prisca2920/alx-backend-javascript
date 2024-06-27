22661a98604bd4d16abb6375b4e82022f356a9a2
prisca2920 <prisca2920@gmail.com>
2024-06-27 13:16:53 +0000

    test file

---
+++ 0x02-ES6_classes/8-main.js
@@ -0,0 +1,9 @@
+import HolbertonClass from "./8-hbtn_class.js";
+
+const hc = new HolbertonClass(12, "Mezzanine")
+console.log(Number(hc));
+console.log(String(hc));import HolbertonClass from "./8-hbtn_class.js";
+
+const hc = new HolbertonClass(12, "Mezzanine")
+console.log(Number(hc));
+console.log(String(hc));
