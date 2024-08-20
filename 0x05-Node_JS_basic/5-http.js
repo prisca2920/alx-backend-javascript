cb39d620eba75599de63b5f345825f3095601136
prisca2920 <prisca2920@gmail.com>
2024-08-20 18:33:07 +0000

    express

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end(); }
+
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      res.end(outString);
+    }).catch(() => {
+      res.statusCode = 404;
+      res.end('Cannot load the database');
+    });
+    }
+});
+
+app.listen(port, host, () => {
+});
+module.exports = app;
