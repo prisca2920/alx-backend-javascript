15b8de37761fa39bf39090a3b1d9fd679d0b4c6c
prisca2920 <prisca2920@gmail.com>
2024-08-23 08:06:56 +0000

    read

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,28 @@
 const fs = require('fs');
 
-function countStudents (path) {
-  
-  
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8').trim();
+    const lines = data.split('\n').slice(1);
+
+    const students = lines.reduce((acc, line) => {
+      const [name, , , field] = line.split(',');
+      if (name && field) {
+        if (!acc[field]) acc[field] = [];
+        acc[field].push(name);
+      }
+      return acc;
+    }, {});
+
+    const totalStudents = lines.length;
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const [field, names] of Object.entries(students)) {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
 module.exports = countStudents;
